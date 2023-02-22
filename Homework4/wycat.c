3d520a6961b5592f65629202317c98f6f6069a22
Marshy8 <bharri27@fish13.eecs.uwyo.edu>
2023-02-21 20:03:47 -0700

    HW4 push

--- Homework4/wycat.c
+++ Homework4/wycat.c
@@ -6,67 +6,76 @@
  * COSC 3750, Homework 4
  *
  * This is a simple version of the cat utility. It is designed to
- * read from files or form the standardinput depending on what the 
+ * read from files or form the standard input depending on what the
  * user puts in before running.
  *
  */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void fileReadWrite(FILE *f)
+void fileReadWrite(FILE *file, char* fileName);
+
+// This is the main function that checks for the
+// differnet input types and sends them to the
+// fileReadWrite function to to the "catting"
+int main(int argc, char **argv)
 {
-char buffer[4096];
-size_t ret, wret;
-do
-{
- ret = fread(buffer, 1, 4096, f);
- if (ret < 4096)
- {
-  if (ferror(f))
-  {
-   fprintf(stderr, "Error reading from: %s\n", f);
-  }
- wret = fwrite(buffer, 1, ret, stdout);
- if (wret != ret)
- {
-  fprintf(stderr, "Error writing to: %s/n", f);
- }
-}
-
-}while (ret == 4096);
-}
-
-
-
-
-int main(int argc, char **argv){
  if (argc == 1)
  {
-  fileReadWrite(stdin);
+  fileReadWrite(stdin, "stdin");
  }
  else
  {
   for(int i=1;i<argc;i++)
   {
-   if(strcmp(argv[i],"-")==0)
+   if(strcmp(argv[i],"-") == 0)
    {
-    fileReadWrite(stdin);
+    fileReadWrite(stdin, "stdin");
    }
    else
    {
-FILE *f = fopen(argv[i],"r");
-    if(!f)
+    FILE *file = fopen(argv[i],"r");
+    if(!file)
     {
-     fprintf(stderr,"%s: %s: No such file or directory\n", argv[0], argv[i]);
+     fprintf(stderr,"%s: Error, no such file or directory: %s\n", argv[0], argv[i]);
     }
     else
     {
-     fileReadWrite(f);
-     fclose(f);
+     fileReadWrite(file, argv[i]);
+     fclose(file);
     }
    }
   }
  }
  return 0;
+}
+
+// The fileReadWrite function does the
+// "catting" to whichever file is passes to it.
+// It also makes sure that it can read from the file
+// and write properly, and display an error if it
+// finds one.
+void fileReadWrite(FILE *file, char* fileName )
+{
+ char buffer[4096];
+ size_t readReturn;
+ size_t writeReturn;
+ do
+ {
+  readReturn = fread(buffer, 1, 4096, file);
+  if (readReturn < 4096)
+  {
+   if (ferror(file))
+   {
+    fprintf(stderr, "Error reading from: %s\n", fileName);
+   }
+   writeReturn = fwrite(buffer, 1, readReturn, stdout);
+  }
+  if (writeReturn != readReturn)
+  {
+   fprintf(stderr, "Error in writing to: %s\n ", fileName);
+  }
+ }while (readReturn == 4096);
 }
